1b9de2e0a31e138c3a83370d08dfcfcd3b1c3eec
leonadervishajj <leona.dervishaj@student.uni-pr.edu>
2026-04-11 13:27:31 +0200

    Mesazhet e derguara nga Klienti

---
+++ MesazhetNgaKlienti.java
@@ -0,0 +1,23 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MesazhetNgaKlienti {
+    private List<String> messages = new ArrayList<>();
+    public void addMessage(String msg){
+        messages.add(msg);
+        saveToFile(msg);
+    }
+    public void saveToFile(String msg){
+        try(FileWriter fw = new FileWriter("mesazhet.log", true)){
+            fw.write(msg + "\n");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public List<String>getMessages(){
+        return messages;
+    }
+}
